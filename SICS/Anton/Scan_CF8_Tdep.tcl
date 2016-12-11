f49c93a237d6d5588e1be53621fd6d4313611fea
taipan <taipan@DAV2-TAIPAN.nbi.ansto.gov.au>
2016-12-12 09:49:29 +1100

    merge from cabin

--- SICS/Anton/Scan_CF8_Tdep.tcl
+++ SICS/Anton/Scan_CF8_Tdep.tcl
@@ -4,43 +4,21 @@
 # TC1 - sample top 
 # TC2 - cold head (should be 10 K below sample top when controlling on both)
 #
-#samplename Octane 
-#sampledescription 99% anhydrous 0.2mm can
+#samplename nano Al2O3 
+#sampledescription full can, 21.2g
 #
-# 80K
+# Base
 #
-sampletitle 80K
+sampletitle Base
+eiscan 70 8 311 100
+#
+# 300K
+#
+sampletitle 300K
 #cold head temperature
-drive tc2_driveable 82
-#hset sample/tc2/sensor/setpoint1 40
-#sample top temperature
-#drive tc1_driveable 40
-wait 900
-runscan ei 70 6.4 319 time 50 datatype {HISTOGRAM_XYT RAW_TOTAL_HISTOGRAM_XE}
+run tc2_driveable 295
+drive tc1_driveable 300
+#wait 900
+eiscan 70 8 311 100
 #
-# 120K
-#
-sampletitle 120K
-#cold head temperature
-drive tc2_driveable 123
-#hset sample/tc2/sensor/setpoint1 40
-#sample top temperature
-#drive tc1_driveable 40
-wait 900
-runscan ei 70 6.4 319 time 50 datatype {HISTOGRAM_XYT RAW_TOTAL_HISTOGRAM_XE}
-#
-# 160K
-#
-sampletitle 160K
-#cold head temperature
-drive tc2_driveable 164
-#hset sample/tc2/sensor/setpoint1 40
-#sample top temperature
-#drive tc1_driveable 40
-wait 900
-runscan ei 70 6.4 319 time 50 datatype {HISTOGRAM_XYT RAW_TOTAL_HISTOGRAM_XE}
-#
-hset sample/tc2/sensor/setpoint1 0
-hset sample/tc1/sensor/setpoint1 0
 drive ei 30
-
