3816a08ad2d93ab5e9f58f00d2aae3fe4642a574
nxi <nxi@ansto.gov.au>
2022-04-27 20:01:02 +1000

    add local log

--- Internal/Experiment/logger.py
+++ Internal/Experiment/logger.py
@@ -7,14 +7,18 @@ from time import strftime, localtime
 __report_folder__ = System.getProperty('sics.data.path') + '/reports'
 __buffer_log_file__ = None
 __history_log_file__ = None
+__local_log_folder__ = System.getProperty('gumtree.log.localpath')
 __buffer_logger__ = None
 __history_logger__ = None
+__local_log_file__ = None
+__local_logger__ = None
 
 
 def init_logger(exp_id = '1001'):
     global __report_folder__, __buffer_log_file__,\
             __history_log_file__, __buffer_logger__,\
-            __history_logger__, logln
+            __history_logger__, __local_log_file__,\
+            __local_logger__, __local_log_folder__
 
     __buffer_log_file__ = __report_folder__ + '/exp' + exp_id
     #globals().get('get_prof_value')('taipan.experiment.id')
@@ -27,22 +31,32 @@ def init_logger(exp_id = '1001'):
     __buffer_log_file__ += '/LogFile.txt'
     __buffer_logger__ = open(__buffer_log_file__, 'a')
     __history_logger__ = open(__history_log_file__, 'a')
+    
+    __local_log_file__ = __local_log_folder__ + '/exp' + exp_id
+    fi = File(__local_log_file__)
+    if not fi.exists():
+        if not fi.mkdirs():
+            print 'Error: failed to make directory: ' + __local_log_file__
+    __local_log_file__ += '/LogFile.txt'
+    __local_logger__ = open(__local_log_file__, 'a')
+    
 
 
 def update_buffer_log_folder(exp_id):
-    global __buffer_log_file__, __report_folder__,\
-            __buffer_logger__, __history_log_file__,\
-            __history_logger__
-    __buffer_log_file__ = __report_folder__ + '/exp' + exp_id
-    #globals().get('get_prof_value')('taipan.experiment.id')
-    fi = File(__buffer_log_file__)
-    if not fi.exists():
-        if not fi.mkdirs():
-            print 'Error: failed to make directory: ' + __buffer_log_file__
-    __history_log_file__ = __buffer_log_file__ + '/History.txt'
-    __buffer_log_file__ += '/LogFile.txt'
-    __buffer_logger__ = open(__buffer_log_file__, 'a')
-    __history_logger__ = open(__history_log_file__, 'a')
+#    global __buffer_log_file__, __report_folder__,\
+#            __buffer_logger__, __history_log_file__,\
+#            __history_logger__
+#    __buffer_log_file__ = __report_folder__ + '/exp' + exp_id
+#    #globals().get('get_prof_value')('taipan.experiment.id')
+#    fi = File(__buffer_log_file__)
+#    if not fi.exists():
+#        if not fi.mkdirs():
+#            print 'Error: failed to make directory: ' + __buffer_log_file__
+#    __history_log_file__ = __buffer_log_file__ + '/History.txt'
+#    __buffer_log_file__ += '/LogFile.txt'
+#    __buffer_logger__ = open(__buffer_log_file__, 'a')
+#    __history_logger__ = open(__history_log_file__, 'a')
+    init_logger(exp_id)
 
 
 
@@ -79,6 +93,23 @@ def log_his(text):
             print 'failed to open history log file: ' + __history_log_file__
             return
         log_his(text)
+
+def log_local(text):
+    global __local_logger__, __local_log_file__
+    try:
+        __local_logger__.write(text + '\n')
+        __local_logger__.flush()
+    except:
+        try:
+            __local_logger__.close()
+        except:
+            pass
+        try:
+            __local_logger__ = open(__local_log_file__, 'a')
+        except:
+            print 'failed to open local log file: ' + __local_log_file__
+            return
+        log_local(text)
     
 def logBook(text):
     from Experiment.config import HISTORY_KEY_WORDS
@@ -87,6 +118,7 @@ def logBook(text):
         tsmp = strftime("[%Y-%m-%d %H:%M:%S]", localtime())
 #        __buffer_logger__.write(tsmp + ' ' + text + '\n')
 #        __buffer_logger__.flush()
+        log_local(tsmp + ' ' + text)
         log_all(tsmp + ' ' + text)
         for item in HISTORY_KEY_WORDS:
             if text.startswith(item):
